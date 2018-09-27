b78e7184fc9a27dfa68600d17dd64a8d42b8e6a1
Andy O'Neill <aoneill01@gmail.com>
2018-09-27 19:41:24 -0400

    Initial commit

---
+++ templates/README.md
@@ -0,0 +1,5 @@
+# {{ projectName }}
+
+## About
+
+*TODO*
---
+++ templates/sketch.ino
@@ -0,0 +1,13 @@
+#include <Gamebuino-Meta.h>
+
+void setup() {
+  gb.begin();
+}    
+
+void loop() {
+  while(!gb.update());
+  gb.display.clear();
+
+  // This is where most of the program takes place
+  gb.display.print("hello, world");
+}
