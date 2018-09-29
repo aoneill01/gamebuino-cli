9b85b848f5d6c11e6723e3dd8b8170db6b01f20b
Andy O'Neill <aoneill01@gmail.com>
2018-09-28 21:56:31 -0400

    Add self-hosted Gamebuino emulator

---
+++ emulator.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const chalk = require('chalk');
+
+const port = 3000;
+
+function emulator(binFile) {
+    const gameFile = path.resolve(process.cwd(), binFile);    
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.redirect('/console.html?bin=game.bin');
+    });
+
+    app.get('/game.bin', (req, res) => {
+        res.sendFile(gameFile);
+    });
+
+    app.use(express.static(path.join('node_modules', '@aoneill01', 'gamebuino-emulator', 'dist')));
+    
+    app.listen(port, () => {
+        console.log(chalk.green(`Emulator running at http://localhost:${port}`));
+    });
+}
+
+module.exports = emulator;
---
+++ initProject.js
@@ -0,0 +1,36 @@
+const { prompt } = require('inquirer');
+const handlebars = require('handlebars');
+const fs = require('fs-extra');
+const path = require('path');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter project name (e.g. SampleGame) ...'
+    }
+];
+
+function initProject() {
+    prompt(questions)
+        .then(answers => {
+            generateProject(answers);
+        });
+}
+
+function generateProject(answers) {
+    writeTemplate('sketch.ino', answers.projectName, `${answers.projectName}.ino`, answers);
+    writeTemplate('README.md', '.', 'README.md', answers);
+    writeTemplate('gitignore', '.', '.gitignore', answers);
+    fs.ensureDirSync(path.join('assets'));
+    fs.ensureDirSync(path.join('binaries', answers.projectName));
+}
+
+function writeTemplate(templateName, destinationDir, destinationName, answers) {
+    const fileContents = fs.readFileSync(path.join(__dirname, 'templates', templateName), 'utf-8').toString();
+    const template = handlebars.compile(fileContents);
+    fs.ensureDir(destinationDir);
+    fs.writeFileSync(path.join(destinationDir, destinationName), template(answers));
+}
+
+module.exports = initProject;
